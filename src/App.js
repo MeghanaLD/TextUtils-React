4bfd5581f662cb47d1e0f3dfde475c599e4e6aa0
Meghana L D <meghana.ld@zapcg.com>
2025-02-13 13:09:04 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
+import About from "./components/About";
+import { use, useState } from "react";
+import Alert from "./components/Alert";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function App() {
+  const [mode, setMode] = useState("light");
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  };
+  const toggleMode = () => {
+    if (mode == "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "#042743";
+      document.body.style.color = "white";
+      showAlert("Dark mode has been enabled", "success");
+      document.title = "TextUtils-Dark Mode";
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+      document.body.style.color = "black";
+      showAlert("Light mode has been enabled", "success");
+      document.title = "TextUtils-Light Mode";
+    }
+  };
+  return (
+    <>
+      <Navbar
+        title="TextUtils"
+        aboutText="About TextUtils"
+        mode={mode}
+        toggleMode={toggleMode}
+      />
+      <Alert alert={alert} />
+      <div className="container my-4">
+        
+        <Routes>
+        <Route path="/" element={<TextForm showAlert={showAlert} heading="Enter the text to analyze" mode={mode} />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+          
+        
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/TextForm.js
@@ -0,0 +1,50 @@
+import React,{useState} from "react";
+
+export default function TextForm(props) {
+    const handleUpClick=()=>{
+        let newText=text.toUpperCase();
+        setText(newText)
+        props.showAlert("Message is Capitalised","success")
+        
+    }
+    const handleDownClick=()=>{
+        let newText=text.toLowerCase();
+        setText(newText)
+        props.showAlert("Message is De-Capitalised","success")
+        
+    }
+    const handleOnChange=(event)=>{
+            setText(event.target.value);
+    }  
+    const handleFocus=()=>{
+        setText(" ")
+        props.showAlert("Text area is cleared","success")
+    }
+    const handleExtraSpaces=()=>{
+        let newText=text.split(/[ ]+/);
+        setText(newText.join(" "))
+        props.showAlert("Extra spaces are removed successfully","success")
+    }
+    const [text,setText]=useState('');
+  return (
+    <>
+    <div className="container"  >
+      <h1>{props.heading}</h1>
+      <div className="mb-3">
+        <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor:props.mode==='dark'?'grey':'white',color:props.mode==='light'?'black':'white'}} id="myBox" rows="12"></textarea>
+      </div>
+      <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to Uppercase</button>
+      <button className="btn btn-primary mx-1" onClick={handleDownClick}>Convert to Lowercase</button>
+      <button className="btn btn-primary mx-1" onClick={handleFocus}>Clear Text</button>
+      <button className="btn btn-primary mx-1" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
+    </div>
+    <div className="container my-3" >
+        <h2>Your text Summary</h2>
+        <p>{text.split(/\s+/).filter(word => word !== "").length} words and {text.length} characters</p>
+        <p>{0.008 * text.split(" ").length}Minutes to read the paragraph</p>
+        <h2>Preview</h2>
+        <p>{text.length>0?text:"Enter something in the text box above to preview it here."}</p>
+    </div>
+    </>
+  );
+}
